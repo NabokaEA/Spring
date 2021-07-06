f85138b785122ef13ff5b21928dd5ebed5421af5
NabokaEA <it_str@amaks-hotels.ru>
2021-07-06 19:55:07 +0200

    Домашнее задание к уроку 5

--- Lesson05_Hibernate/src/main/java/ru/geekbrains/App.java
+++ Lesson05_Hibernate/src/main/java/ru/geekbrains/App.java
@@ -3,19 +3,27 @@ package ru.geekbrains;
 
 import ru.geekbrains.entity.Product;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
 
         ProductDAO productDAO = new ProductDAO();
         Product product = new Product(null, "pen", 100);
+        List<Product> products = List.of(
+                new Product(null, "pen", 100),
+                new Product(null, "pen", 100),
+                new Product(null, "pen", 100),
+                new Product(null, "pen", 100),
+                new Product(null, "pen", 100)
+        );
         productDAO.saveOrUpdate(product);
-        Product findProduct=productDAO.findById(1L);
+        Product findProduct = productDAO.findById(1L);
         System.out.println(findProduct);
 
         productDAO.entityManager.close();
     }
-
 
 
 }
