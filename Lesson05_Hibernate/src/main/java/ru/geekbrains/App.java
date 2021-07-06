484e4e501735929da90531afb46cd553c9f7f4b4
NabokaEA <it_str@amaks-hotels.ru>
2021-07-06 19:34:59 +0200

    Домашнее задание к уроку 5

---
+++ Lesson05_Hibernate/src/main/java/ru/geekbrains/App.java
@@ -0,0 +1,26 @@
+package ru.geekbrains;
+
+
+import ru.geekbrains.entity.Product;
+
+public class App {
+    public static void main(String[] args) {
+
+
+        ProductDAO productDAO = new ProductDAO();
+        Product product = new Product(null, "pen", 100);
+        productDAO.saveOrUpdate(product);
+        Product findProduct=productDAO.findById(1L);
+        System.out.println(findProduct);
+    }
+
+
+
+}
+
+
+
+
+
+
+
---
+++ Lesson05_Hibernate/src/main/java/ru/geekbrains/ProductDAO.java
@@ -0,0 +1,33 @@
+package ru.geekbrains;
+
+import org.hibernate.cfg.Configuration;
+import ru.geekbrains.entity.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class ProductDAO {
+    EntityManagerFactory eMFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    EntityManager entityManager = eMFactory.createEntityManager();
+
+    public void saveOrUpdate(Product product) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(product);
+        entityManager.getTransaction().commit();
+    }
+
+    public Product findById(Long id) {
+        Product product = entityManager.find(Product.class, id);
+        return product;
+    }
+/*
+    public List<Product> findAll() {
+
+    }
+*/
+    public void deleteById(Long id) {
+
+    }
+
+}
