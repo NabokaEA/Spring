de6491ef67caa0877d2379fac32a934397858d41
NabokaEA <it_str@amaks-hotels.ru>
2021-07-06 19:59:57 +0200

    Домашнее задание к уроку 5

--- Lesson05_Hibernate/src/main/java/ru/geekbrains/App.java
+++ Lesson05_Hibernate/src/main/java/ru/geekbrains/App.java
@@ -11,13 +11,13 @@ public class App {
 
         ProductDAO productDAO = new ProductDAO();
         Product product = new Product(null, "pen", 100);
-        List<Product> products = List.of(
-                new Product(null, "pen", 100),
-                new Product(null, "pen", 100),
-                new Product(null, "pen", 100),
-                new Product(null, "pen", 100),
-                new Product(null, "pen", 100)
-        );
+        List.of(
+                new Product(null, "pensil", 100),
+                new Product(null, "pen", 400),
+                new Product(null, "plate", 500),
+                new Product(null, "hen", 300),
+                new Product(null, "hat", 700)
+        ).forEach(productDAO.entityManager::persist);
         productDAO.saveOrUpdate(product);
         Product findProduct = productDAO.findById(1L);
         System.out.println(findProduct);
